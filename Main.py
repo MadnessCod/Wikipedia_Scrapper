8ef63cbdaf4357a8ad1ac35ba1c4847ab96ddafe
Sadra Hosseini <sadrahosseini98@gmail.com>
2024-04-25 20:48:05 +0330

    find h1

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
 import requests
-
+from bs4 import BeautifulSoup
 
 try:
     text = requests.get('https://en.wikipedia.org/wiki/', timeout=20)
 except requests.exceptions.RequestException as e:
     print(f'Error: {e}')
-
+else:
+    soup = BeautifulSoup(text.text, 'html.parser')
+    print(soup.find('h1'))
