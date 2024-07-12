9890d3302e3f3dcb6a10c5e602b6cc7daaf508ea
Sadra Hosseini <sadrahosseini98@gmail.com>
2024-07-12 10:20:52 -0800

    modified Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
 
-try:
-    text = requests.get('https://en.wikipedia.org/wiki/', timeout=20)
-except requests.exceptions.RequestException as e:
-    print(f'Error: {e}')
-else:
-    soup = BeautifulSoup(text.text, 'html.parser')
-    print(soup.find('h1'))
+
+class Scrapper:
+    def __init__(self, url):
+        self.url = url
+        self.links = list()
+
+    def request(self):
+        try:
+            self.response = requests.get(self.url)
+        except requests.exceptions.RequestException as error:
+            print(f'Requests Error: {error}')
+        else:
+            self.home_page()
+
+    def home_page(self):
+        soup = BeautifulSoup(self.response.text, 'html.parser')
+        try:
+            upper = soup.find('div', id='mp-upper')
+            middle = soup.find('div', id='mp-middle')
+            lower = soup.find('div', id='mp-lower')
+        except AttributeError as error:
+            print(f'AttributeError: {error}')
+        else:
+            for div in upper.find_all('div'):
+                if div.id == 'mp-left':
+                    featured_article = div.find('p')
+                    for link in featured_article.find_all('a'):
+                        self.links.append(self.url + link.get('href'))
+                        text = featured_article.text
+                    return text, self.links
+
+    def download(self):
+        pass
+
+
+if __name__ == '__main__':
+    home_page = Scrapper('https://en.wikipedia.org/wiki/Main_Page')
+    print(home_page.request())
